aa6d95e5e3c2920eb2d631b8da0d87ddecd1fc23
pezo <pezo@d1c6ea46-091f-4b3d-9d77-b4eb87a02c39>
2014-04-13 21:59:22 +0000

    * stm/impy: Changed HAL LL USB source file to not be excluded from build.             Added usbd_conf header and source files.             Added USB OTG FS interrupt handler.

---
+++ trunk/stm/include/usbd_conf.h
@@ -0,0 +1,59 @@
+/**
+ * @file    usbd_conf.h
+ * @author  Peter Feichtinger
+ * @date    13.04.2014
+ * @brief   General low level USB driver configuration.
+ */
+
+#ifndef USBD_CONF_H_
+#define USBD_CONF_H_
+
+// Includes -------------------------------------------------------------------
+#include "stm32f4xx_hal.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Constants ------------------------------------------------------------------
+#define USBD_MAX_NUM_INTERFACES               1
+#define USBD_MAX_NUM_CONFIGURATION            1
+#define USBD_MAX_STR_DESC_SIZ                 0x100
+#define USBD_SUPPORT_USER_STRING              0
+#define USBD_SELF_POWERED                     1
+#define USBD_DEBUG_LEVEL                      0
+
+// Macros ---------------------------------------------------------------------
+/* Memory management macros */
+#define USBD_malloc         malloc
+#define USBD_free           free
+#define USBD_memset         memset
+#define USBD_memcpy         memcpy
+    
+/* DEBUG macros */
+#if (USBD_DEBUG_LEVEL > 0)
+#define  USBD_UsrLog(...)   printf(__VA_ARGS__);\
+                            printf("\n");
+#else
+#define USBD_UsrLog(...)
+#endif
+
+#if (USBD_DEBUG_LEVEL > 1)
+
+#define  USBD_ErrLog(...)   printf("ERROR: ") ;\
+                            printf(__VA_ARGS__);\
+                            printf("\n");
+#else
+#define USBD_ErrLog(...)
+#endif
+
+#if (USBD_DEBUG_LEVEL > 2)
+#define  USBD_DbgLog(...)   printf("DEBUG : ") ;\
+                            printf(__VA_ARGS__);\
+                            printf("\n");
+#else
+#define USBD_DbgLog(...)
+#endif
+
+// ----------------------------------------------------------------------------
+
+#endif /* USBD_CONF_H_ */
