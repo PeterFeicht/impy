ea3b954fad521885f46186b96fb0a3509bf37db7
Peter Feichtinger <shippo@gmx.at>
2014-04-13 22:15:32 +0000

    * stm/impy: Added mx_init header and source files for MX generated hardware initialization code.

---
+++ stm/include/mx_init.h
@@ -0,0 +1,22 @@
+/**
+ * @file    mx_init.h
+ * @author  Peter
+ * @date    14.04.2014
+ * @brief   
+ * 
+ * 
+ */
+
+#ifndef MX_INIT_H_
+#define MX_INIT_H_
+
+// Includes -------------------------------------------------------------------
+#include "stm32f4xx_hal.h"
+
+// External variables ---------------------------------------------------------
+
+
+// Exported functions ---------------------------------------------------------
+void MX_Init(void);
+
+#endif /* MX_INIT_H_ */
