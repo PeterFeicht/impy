8af4286cacd2980b0ebe6a25d68508bb712255de
pezo <pezo@d1c6ea46-091f-4b3d-9d77-b4eb87a02c39>
2014-04-14 11:01:21 +0000

    * stm/impy: Renamed everything in usbd_vcp header and source files from CDC to VCP, plus some formatting fu.

--- trunk/stm/include/usbd_vcp.h
+++ trunk/stm/include/usbd_vcp.h
@@ -1,171 +1,88 @@
 /**
-  ******************************************************************************
-  * @file    usbd_cdc_core.h
-  * @author  MCD Application Team
-  * @version V2.0.0
-  * @date    18-February-2014
-  * @brief   header file for the usbd_cdc_core.c file.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */ 
+ * @file    usbd_vcp.h
+ * @author  Peter Feichtinger
+ * @date    14.04.2014
+ * @brief   Header file for the usbd_vcp class.
+ * 
+ * This file is mostly adapted from usbd_cdc.h from the STM32CubeF4 firmware package by ST.
+ */
 
-/* Includes ------------------------------------------------------------------*/
 
-#ifndef __USB_CDC_CORE_H_
-#define __USB_CDC_CORE_H_
+#ifndef USBD_VCP_H_
+#define USBD_VCP_H_
 
+// Includes -------------------------------------------------------------------
 #include  "usbd_ioreq.h"
 
-/** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
-  * @{
-  */
-  
-/** @defgroup usbd_cdc
-  * @brief This file is the Header file for USBD_cdc.c
-  * @{
-  */ 
+// Constants ------------------------------------------------------------------
+#define VCP_IN_EP       0x81    /* EP1 for data IN */
+#define VCP_OUT_EP      0x01    /* EP1 for data OUT */
+#define VCP_CMD_EP      0x82    /* EP2 for CDC commands */
 
+// VCP Endpoints parameters: you can fine tune these values depending on the needed baudrates and performance.
+#define VCP_DATA_HS_MAX_PACKET_SIZE        512  /* Endpoint IN & OUT Packet size */
+#define VCP_DATA_FS_MAX_PACKET_SIZE         64  /* Endpoint IN & OUT Packet size */
+#define VCP_CMD_PACKET_SIZE                  8  /* Control Endpoint Packet size */ 
 
-/** @defgroup usbd_cdc_Exported_Defines
-  * @{
-  */ 
-#define CDC_IN_EP                       0x81  /* EP1 for data IN */
-#define CDC_OUT_EP                      0x01  /* EP1 for data OUT */
-#define CDC_CMD_EP                      0x82  /* EP2 for CDC commands */
+#define USB_VCP_CONFIG_DESC_SIZ             67
+#define VCP_DATA_HS_IN_PACKET_SIZE          VCP_DATA_HS_MAX_PACKET_SIZE
+#define VCP_DATA_HS_OUT_PACKET_SIZE         VCP_DATA_HS_MAX_PACKET_SIZE
+#define VCP_DATA_FS_IN_PACKET_SIZE          VCP_DATA_FS_MAX_PACKET_SIZE
+#define VCP_DATA_FS_OUT_PACKET_SIZE         VCP_DATA_FS_MAX_PACKET_SIZE
 
-/* CDC Endpoints parameters: you can fine tune these values depending on the needed baudrates and performance. */
-#define CDC_DATA_HS_MAX_PACKET_SIZE        512  /* Endpoint IN & OUT Packet size */
-#define CDC_DATA_FS_MAX_PACKET_SIZE         64  /* Endpoint IN & OUT Packet size */
-#define CDC_CMD_PACKET_SIZE                  8  /* Control Endpoint Packet size */ 
+// VCP Definitions
+#define VCP_SEND_ENCAPSULATED_COMMAND       0x00
+#define VCP_GET_ENCAPSULATED_RESPONSE       0x01
+#define VCP_SET_COMM_FEATURE                0x02
+#define VCP_GET_COMM_FEATURE                0x03
+#define VCP_CLEAR_COMM_FEATURE              0x04
+#define VCP_SET_LINE_CODING                 0x20
+#define VCP_GET_LINE_CODING                 0x21
+#define VCP_SET_CONTROL_LINE_STATE          0x22
+#define VCP_SEND_BREAK                      0x23
 
-#define USB_CDC_CONFIG_DESC_SIZ                67
-#define CDC_DATA_HS_IN_PACKET_SIZE                CDC_DATA_HS_MAX_PACKET_SIZE
-#define CDC_DATA_HS_OUT_PACKET_SIZE               CDC_DATA_HS_MAX_PACKET_SIZE
-
-#define CDC_DATA_FS_IN_PACKET_SIZE                CDC_DATA_FS_MAX_PACKET_SIZE
-#define CDC_DATA_FS_OUT_PACKET_SIZE               CDC_DATA_FS_MAX_PACKET_SIZE
-
-/*---------------------------------------------------------------------*/
-/*  CDC definitions                                                    */
-/*---------------------------------------------------------------------*/
-
-#define CDC_SEND_ENCAPSULATED_COMMAND               0x00
-#define CDC_GET_ENCAPSULATED_RESPONSE               0x01
-#define CDC_SET_COMM_FEATURE                        0x02
-#define CDC_GET_COMM_FEATURE                        0x03
-#define CDC_CLEAR_COMM_FEATURE                      0x04
-#define CDC_SET_LINE_CODING                         0x20
-#define CDC_GET_LINE_CODING                         0x21
-#define CDC_SET_CONTROL_LINE_STATE                  0x22
-#define CDC_SEND_BREAK                              0x23
-
-/**
-  * @}
-  */ 
-
-
-/** @defgroup USBD_CORE_Exported_TypesDefinitions
-  * @{
-  */
-
-/**
-  * @}
-  */ 
+// Exported type definitions --------------------------------------------------
 typedef struct
 {
-  uint32_t bitrate;
-  uint8_t  format;
-  uint8_t  paritytype;
-  uint8_t  datatype;
-}USBD_CDC_LineCodingTypeDef;
+    uint32_t bitrate;
+    uint8_t  format;
+    uint8_t  paritytype;
+    uint8_t  datatype;
+} USBD_VCP_LineCodingTypeDef;
 
-typedef struct _USBD_CDC_Itf
+typedef struct
 {
-  int8_t (* Init)          (void);
-  int8_t (* DeInit)        (void);
-  int8_t (* Control)       (uint8_t, uint8_t * , uint16_t);   
-  int8_t (* Receive)       (uint8_t *, uint32_t *);  
-
-}USBD_CDC_ItfTypeDef;
+    int8_t (*Init)      (void);
+    int8_t (*DeInit)    (void);
+    int8_t (*Control)   (uint8_t, uint8_t*, uint16_t);
+    int8_t (*Receive)   (uint8_t*, uint32_t*);
+} USBD_VCP_ItfTypeDef;
 
 
 typedef struct
 {
-  uint32_t data[CDC_DATA_HS_MAX_PACKET_SIZE/4];      /* Force 32bits alignment */
-  uint8_t  CmdOpCode;
-  uint8_t  CmdLength;    
-  uint8_t  *RxBuffer;  
-  uint8_t  *TxBuffer;   
-  uint32_t RxLength;
-  uint32_t TxLength;    
-  
-  __IO uint32_t TxState;     
-  __IO uint32_t RxState;    
-}
-USBD_CDC_HandleTypeDef; 
+    uint32_t data[VCP_DATA_HS_MAX_PACKET_SIZE/4];
+    uint8_t  CmdOpCode;
+    uint8_t  CmdLength;    
+    uint8_t  *RxBuffer;  
+    uint8_t  *TxBuffer;   
+    uint32_t RxLength;
+    uint32_t TxLength;    
+    
+    __IO uint32_t TxState;     
+    __IO uint32_t RxState;    
+} USBD_VCP_HandleTypeDef; 
 
+// Exported variables ---------------------------------------------------------
+extern USBD_ClassTypeDef  USBD_VCP;
 
+// Exported functions ---------------------------------------------------------
+uint8_t USBD_VCP_RegisterInterface(USBD_HandleTypeDef *pdev, USBD_VCP_ItfTypeDef *fops);
+uint8_t USBD_VCP_SetTxBuffer(USBD_HandleTypeDef *pdev, uint8_t *pbuff, uint16_t length);
+uint8_t USBD_VCP_SetRxBuffer(USBD_HandleTypeDef *pdev, uint8_t *pbuff);
+uint8_t USBD_VCP_ReceivePacket(USBD_HandleTypeDef *pdev);
+uint8_t USBD_VCP_TransmitPacket(USBD_HandleTypeDef *pdev);
 
-/** @defgroup USBD_CORE_Exported_Macros
-  * @{
-  */ 
-  
-/**
-  * @}
-  */ 
+// ----------------------------------------------------------------------------
 
-/** @defgroup USBD_CORE_Exported_Variables
-  * @{
-  */ 
-
-extern USBD_ClassTypeDef  USBD_CDC;
-/**
-  * @}
-  */ 
-
-/** @defgroup USB_CORE_Exported_Functions
-  * @{
-  */
-uint8_t  USBD_CDC_RegisterInterface  (USBD_HandleTypeDef   *pdev, 
-                                      USBD_CDC_ItfTypeDef *fops);
-
-uint8_t  USBD_CDC_SetTxBuffer  (USBD_HandleTypeDef   *pdev,
-                                uint8_t  *pbuff,
-                                uint16_t length);
-
-uint8_t  USBD_CDC_SetRxBuffer        (USBD_HandleTypeDef   *pdev,
-                                      uint8_t  *pbuff);
-  
-uint8_t  USBD_CDC_ReceivePacket  (USBD_HandleTypeDef *pdev);
-
-uint8_t  USBD_CDC_TransmitPacket  (USBD_HandleTypeDef *pdev);
-/**
-  * @}
-  */ 
-
-#endif  // __USB_CDC_CORE_H_
-/**
-  * @}
-  */ 
-
-/**
-  * @}
-  */ 
-  
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+#endif /* USBD_VCP_H_ */
