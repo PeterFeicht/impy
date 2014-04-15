2f85154fdc1e471d389ede93be7f426163a2d412
pezo <pezo@d1c6ea46-091f-4b3d-9d77-b4eb87a02c39>
2014-04-15 21:24:22 +0000

    * stm/impy: (ad5933.h) Change signature of AD5933_MeasureImpedance to add an AD5933_ImpedanceData pointer for the destination of the measurement.

--- trunk/stm/include/ad5933.h
+++ trunk/stm/include/ad5933.h
@@ -376,7 +376,7 @@ typedef struct
 AD5933_Status AD5933_GetStatus(void);
 AD5933_Error AD5933_Init(I2C_HandleTypeDef *i2c);
 AD5933_Error AD5933_Reset(void);
-AD5933_Error AD5933_MeasureImpedance(AD5933_Sweep *sweep);
+AD5933_Error AD5933_MeasureImpedance(AD5933_Sweep *sweep, AD5933_ImpedanceData *buffer);
 AD5933_Error AD5933_MeasureTemperature(float *destination);
 AD5933_Error AD5933_Calibrate(AD5933_GainFactorData *data);
 void AD5933_TIM_PeriodElapsedCallback(void);
