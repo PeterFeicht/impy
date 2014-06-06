b96cbc3a984a08ca9efd67b639b11790665289a5
pezo <pezo@d1c6ea46-091f-4b3d-9d77-b4eb87a02c39>
2014-06-06 12:08:00 +0000

    * stm/impy: Add header file with English string definitions (strings_en.h) to be used in the console driver (and maybe Ethernet as well).

---
+++ trunk/stm/include/strings_en.h
@@ -0,0 +1,30 @@
+/**
+ * @file    strings_en.h
+ * @author  Peter
+ * @date    06.06.2014
+ * @brief   Defines user interface strings in English.
+ */
+
+#ifndef STRINGS_EN_H_
+#define STRINGS_EN_H_
+
+// Console strings ------------------------------------------------------------
+const char *txtErrArgNum = "Wrong number of arguments.\r\n";
+const char *txtErrNoSubcommand = "Missing command, type 'help' for possible commands.\r\n";
+const char *txtUnknownTopic = "Unknown help topic, type 'help' for possible commands.\r\n";
+const char *txtUnknownCommand = "Unknown command.\r\n";
+const char *txtUnknownSubcommand = "Unknown subcommand.\r\n";
+const char *txtNotImplemented = "Not yet implemented.\r\n";
+const char *txtUnknownOption = "Unknown option.\r\n";
+const char *txtSetOnlyWhenAutorangeDisabled = "This value can only be set when autoranging is disabled.\r\n";
+const char *txtGetOnlyWhenAutorangeDisabled = "This value can only be read when autoranging is disabled.\r\n";
+
+// Words ----------------------------------------------------------------------
+const char *txtEnabled = "enabled";
+const char *txtDisabled = "disabled";
+const char *txtOn = "on";
+const char *txtOff = "off";
+
+// ----------------------------------------------------------------------------
+
+#endif /* STRINGS_EN_H_ */
