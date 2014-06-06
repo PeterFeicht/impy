463bde0c4e2da133341c436ecfcf159426d80601
pezo <pezo@d1c6ea46-091f-4b3d-9d77-b4eb87a02c39>
2014-06-06 13:24:20 +0000

    * stm/impy: Add util.c source file and implement IntFromSiString function.

---
+++ trunk/stm/src/util.c
@@ -0,0 +1,96 @@
+/**
+ * @file    util.c
+ * @author  Peter
+ * @date    06.06.2014
+ * @brief   This file contains utility functions for things like converting strings to numbers and such.
+ */
+
+// Includes -------------------------------------------------------------------
+#include <string.h>
+#include <ctype.h>
+#include "util.h"
+
+// Exported functions ---------------------------------------------------------
+
+/**
+ * Convert an integer value from a string with possible SI suffix (like {@code 100k}).
+ * 
+ * The string supplied to this function may not contain space characters inside the number, leading white space and
+ * zeros are ignored. Parsing of the value is stopped at the first space character, so the string need not be
+ * terminated.
+ * 
+ * If not {@code NULL}, the variable pointed to by {@code end} will receive the address of the first character after
+ * the converted number, or {@code NULL} in case of an error.
+ * 
+ * Possible SI suffixes for this function are:
+ *  + {@code SI_PREFIX_KILO} = k
+ *  + {@code SI_PREFIX_MEGA} = M
+ * 
+ * @param str Pointer to a string containing a numeric value
+ * @param end Pointer to a variable receiving the position of the first character after the number, or {@code NULL}
+ * @return The converted numeric value, or {@code 0} in case of an error
+ */
+uint32_t IntFromSiString(const char *str, const char **end)
+{
+    uint32_t val = 0;
+    const char *pos = str;
+    
+    if(str == NULL)
+    {
+        if(end != NULL)
+            *end = NULL;
+        return 0;
+    } 
+    
+    // Ignore leading white space and zeros
+    while(isspace((unsigned char)*pos) || *pos == '0')
+    {
+        pos++;
+    }
+    
+    // Convert numeric part
+    while(isdigit((unsigned char)*pos))
+    {
+        val = val * 10 + (*pos - '0');
+        pos++;
+    }
+    
+    // No suffix
+    if(isspace((unsigned char)*pos) || *pos == 0)
+    {
+        if(end != NULL)
+            *end = pos;
+        return val;
+    }
+    
+    // Check for valid suffix
+    switch(*pos)
+    {
+        case SI_PREFIX_KILO:
+            val *= 1000;
+            break;
+        case SI_PREFIX_MEGA:
+            val *= 1000000;
+            break;
+        default:
+            if(end != NULL)
+                *end = NULL;
+            return 0;
+    }
+    
+    pos++;
+    if(isspace((unsigned char)*pos) || *pos == 0)
+    {
+        if(end != NULL)
+            *end = pos;
+        return val;
+    }
+    else
+    {
+        if(end != NULL)
+            *end = NULL;
+        return 0;
+    }
+}
+
+// ----------------------------------------------------------------------------
