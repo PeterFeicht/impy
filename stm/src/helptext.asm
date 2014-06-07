982f927fa3e8f139b135aa2d8417615089950728
Peter Feichtinger <shippo@gmx.at>
2014-06-07 19:26:07 +0000

    * stm/impy: (helptext.asm) Add note that this file needs to be rebuilt manually when changed.

--- stm/src/helptext.asm
+++ stm/src/helptext.asm
@@ -3,6 +3,9 @@
  * @author  Peter Feichtinger
  * @date    25.05.2014
  * @brief   Includes the command line help text from command-line.txt file.
+ * 
+ * Note that this file needs to be manually rebuilt when command-line.txt changes since the Eclipse build system
+ * doesn't notice that.
  */
  
     .section .data.helptext_start
