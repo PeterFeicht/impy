6a4961f9b307ea22f5667b2eb4d828ed133c3722
pezo <pezo@d1c6ea46-091f-4b3d-9d77-b4eb87a02c39>
2014-06-08 15:07:34 +0000

    * stm/impy: (strings_en.h) Add strings for 'board set' command.

--- trunk/stm/include/strings_en.h
+++ trunk/stm/include/strings_en.h
@@ -9,6 +9,7 @@
 #define STRINGS_EN_H_
 
 // Console strings ------------------------------------------------------------
+// XXX Maybe there should be some kind of error code prefix for parsing by PC software
 const char *txtErrArgNum = "Wrong number of arguments.\r\n";
 const char *txtErrNoSubcommand = "Missing command, type 'help' for possible commands.\r\n";
 const char *txtUnknownTopic = "Unknown help topic, type 'help' for possible commands.\r\n";
@@ -16,7 +17,10 @@ const char *txtUnknownCommand = "Unknown command.\r\n";
 const char *txtUnknownSubcommand = "Unknown subcommand.\r\n";
 const char *txtNotImplemented = "Not yet implemented.\r\n";
 const char *txtUnknownOption = "Unknown option.\r\n";
-const char *txtSetOnlyWhenAutorangeDisabled = "This value can only be set when autoranging is disabled.\r\n";
+const char *txtInvalidValue = "Invalid value for this argument: ";
+const char *txtSetOnlyWhenIdle = "This value cannot be set while a sweep is running: ";
+const char *txtEffectiveNextSweep = "Autorange setting will take effect on the next sweep.\r\n";
+const char *txtSetOnlyWhenAutorangeDisabled = "This value can only be set when autoranging is disabled: ";
 const char *txtGetOnlyWhenAutorangeDisabled = "This value can only be read when autoranging is disabled.\r\n";
 
 // Words ----------------------------------------------------------------------
