1ab5066cd088d396f65c7589b76380d7e4446faa
pezo <pezo@d1c6ea46-091f-4b3d-9d77-b4eb87a02c39>
2014-06-08 15:16:31 +0000

    * stm/impy: (strings_en.h) Remove trailing line breaks from all strings, we can use the VCP_SendLine function if we need a line break after.   (console.c) Replace VCP_SendString calls by VCP_SendLine for all changed strings.

--- trunk/stm/include/strings_en.h
+++ trunk/stm/include/strings_en.h
@@ -10,18 +10,18 @@
 
 // Console strings ------------------------------------------------------------
 // XXX Maybe there should be some kind of error code prefix for parsing by PC software
-const char *txtErrArgNum = "Wrong number of arguments.\r\n";
-const char *txtErrNoSubcommand = "Missing command, type 'help' for possible commands.\r\n";
-const char *txtUnknownTopic = "Unknown help topic, type 'help' for possible commands.\r\n";
-const char *txtUnknownCommand = "Unknown command.\r\n";
-const char *txtUnknownSubcommand = "Unknown subcommand.\r\n";
-const char *txtNotImplemented = "Not yet implemented.\r\n";
-const char *txtUnknownOption = "Unknown option.\r\n";
+const char *txtErrArgNum = "Wrong number of arguments.";
+const char *txtErrNoSubcommand = "Missing command, type 'help' for possible commands.";
+const char *txtUnknownTopic = "Unknown help topic, type 'help' for possible commands.";
+const char *txtUnknownCommand = "Unknown command.";
+const char *txtUnknownSubcommand = "Unknown subcommand.";
+const char *txtNotImplemented = "Not yet implemented.";
+const char *txtUnknownOption = "Unknown option.";
 const char *txtInvalidValue = "Invalid value for this argument: ";
 const char *txtSetOnlyWhenIdle = "This value cannot be set while a sweep is running: ";
-const char *txtEffectiveNextSweep = "Autorange setting will take effect on the next sweep.\r\n";
+const char *txtEffectiveNextSweep = "Autorange setting will take effect on the next sweep.";
 const char *txtSetOnlyWhenAutorangeDisabled = "This value can only be set when autoranging is disabled: ";
-const char *txtGetOnlyWhenAutorangeDisabled = "This value can only be read when autoranging is disabled.\r\n";
+const char *txtGetOnlyWhenAutorangeDisabled = "This value can only be read when autoranging is disabled.";
 
 // Words ----------------------------------------------------------------------
 const char *txtEnabled = "enabled";
