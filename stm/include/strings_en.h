6bac8c362050894991b171dfc515816ac86ac7c5
Peter Feichtinger <shippo@gmx.at>
2014-06-09 13:25:54 +0000

    * stm/impy: (strings_en.h) Add strings for 'board info' command.

--- stm/include/strings_en.h
+++ stm/include/strings_en.h
@@ -8,6 +8,10 @@
 #ifndef STRINGS_EN_H_
 #define STRINGS_EN_H_
 
+// Constants ------------------------------------------------------------------
+#define THIS_IS_IMPY    "This is impy version "
+#define BUILT_ON        " built on "
+
 // Console strings ------------------------------------------------------------
 // XXX Maybe there should be some kind of error code prefix for parsing by PC software
 const char *txtErrArgNum = "Wrong number of arguments.";
@@ -32,6 +36,16 @@ const char *txtAdStatusFinish = "Measurement finished, points measured: ";
 const char *txtAdStatusCalibrate = "Calibration measurement is running.";
 const char *txtAutorangeStatus = "Autoranging is ";
 const char *txtLastInterrupted = "The last measurement was interrupted.";
+// board info
+const char *txtAdStatus = "AD5933 driver status: ";
+const char *txtAdStatusMeasureImpedance = "Impedance measurement is running.";
+const char *txtPortsAvailable = "Ports available for measurements: ";
+const char *txtAttenuationsAvailable = "Possible voltage attenuation factors: ";
+const char *txtFeedbackResistorValues = "Feedback resistor values: ";
+const char *txtCalibrationValues = "Calibration resistor values: ";
+const char *txtNotInstalled = " not installed.";
+const char *txtNoMemory = "\r\nNo external memory installed.";
+const char *txtFrequencyRange = "The possible frequency range in Hz is ";
 
 // Words ----------------------------------------------------------------------
 const char *txtEnabled = "enabled";
