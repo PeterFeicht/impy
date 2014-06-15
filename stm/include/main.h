6d1eb6826f2b2b15280b19b81bd50d79f6454b13
Peter Feichtinger <shippo@gmx.at>
2014-06-15 20:01:41 +0000

    * stm/impy: (main.h) Add constants for the SPI definition. Whitespace changes.

--- stm/include/main.h
+++ stm/include/main.h
@@ -38,64 +38,88 @@ typedef struct
 } Board_Status;
 
 // Constants ------------------------------------------------------------------
-#define BOARD_VERSION   "1.0"
+#define BOARD_VERSION                   "1.0"
 
-#define LED_PORT        GPIOD
-#define LED_ORANGE      GPIO_PIN_13
-#define LED_GREEN       GPIO_PIN_12
-#define LED_RED         GPIO_PIN_14
-#define LED_BLUE        GPIO_PIN_15
+#define LED_PORT                        GPIOD
+#define LED_ORANGE                      GPIO_PIN_13
+#define LED_GREEN                       GPIO_PIN_12
+#define LED_RED                         GPIO_PIN_14
+#define LED_BLUE                        GPIO_PIN_15
 
 /**
  * The minimum port number that can be used for measurements.
  */
-#define PORT_MIN        0
+#define PORT_MIN                        0
 /**
  * The maximum port number that can be used for measurements.
  */
-#define PORT_MAX        9
+#define PORT_MAX                        9
 
 /**
  * The first calibration port number
  */
-#define CAL_PORT_MIN    10
+#define CAL_PORT_MIN                    (PORT_MAX + 1)
 
 /**
  * The minimum frequency that can be measured.
  */
-#define FREQ_MIN        100
+#define FREQ_MIN                        100
 /**
  * The maximum frequency that can be measured.
  */
-#define FREQ_MAX        100000
+#define FREQ_MAX                        100000
 
-// Values of the calibration resistors
 /**
  * @defgroup CAL_PORT Calibration Resistor Values
  * @{
  */
-#define CAL_PORT_10         10              //!< Port 10 value: 10
-#define CAL_PORT_11         100             //!< Port 11 value: 100
-#define CAL_PORT_12         1000            //!< Port 12 value: 1k
-#define CAL_PORT_13         10000           //!< Port 13 value: 10k
-#define CAL_PORT_14         100000          //!< Port 14 value: 100k
-#define CAL_PORT_15         1000000         //!< Port 15 value: 1M
+#define CAL_PORT_10                     10              //!< Port 10 value: 10
+#define CAL_PORT_11                     100             //!< Port 11 value: 100
+#define CAL_PORT_12                     1000            //!< Port 12 value: 1k
+#define CAL_PORT_13                     10000           //!< Port 13 value: 10k
+#define CAL_PORT_14                     100000          //!< Port 14 value: 100k
+#define CAL_PORT_15                     1000000         //!< Port 15 value: 1M
+/** @} */
+
+/**
+ * Timeout in ms for SPI communication
+ */
+#define BOARD_SPI_TIMEOUT               10
+
+/**
+ * @defgroup BOARD_SPI_SS_GPIO SPI Slave Select GPIO Definition
+ * @{
+ */
+#define BOARD_SPI_SS_GPIO_PORT          PORTD
+#define BOARD_SPI_SS_GPIO_FLASH         GPIO_PIN_0
+#define BOARD_SPI_SS_GPIO_SRAM          GPIO_PIN_1
+#define BOARD_SPI_SS_GPIO_MUX           GPIO_PIN_2
+/** @} */
+
+/**
+ * @defgroup AD725 Communication Definition for the AD725
+ * @{
+ */
+#define AD725_MASK_PORT                 ((uint8_t)0x0F)
+#define AD725_CHIP_ENABLE_NOT           ((uint8_t)0x80)
+#define AD725_CHIP_CSA_NOT              ((uint8_t)0x40)
+#define AD725_CHIP_CSB_NOT              ((uint8_t)0x20)
 /** @} */
 
 // Uncomment if the EEPROM is fitted
-//#define BOARD_HAS_EEPROM        1
+//#define BOARD_HAS_EEPROM                1
 
 // Uncomment if the SRAM is fitted
-//#define BOARD_HAS_SRAM          1
+//#define BOARD_HAS_SRAM                  1
 
 // Uncomment if the Flash memory is fitted
-//#define BOARD_HAS_FLASH         1
+//#define BOARD_HAS_FLASH                 1
 
 // Uncomment if the Ethernet interface is fitted
-//#define BOARD_HAS_ETHERNET      1
+//#define BOARD_HAS_ETHERNET              1
 
 // Uncomment if the USB host port is fitted
-//#define BOARD_HAS_USBH          1
+//#define BOARD_HAS_USBH                  1
 
 // Exported functions ---------------------------------------------------------
 Board_Error Board_SetStartFreq(uint32_t freq);
