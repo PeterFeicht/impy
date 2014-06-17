12faba640974ce77acf1284dc86ac78e3fbe6094
pezo <pezo@d1c6ea46-091f-4b3d-9d77-b4eb87a02c39>
2014-06-17 09:12:35 +0000

    * stm/impy: (mx_init.c) Change I2C clock speed to 400kHz (fastaah is bettaah).

--- trunk/stm/src/mx_init.c
+++ trunk/stm/src/mx_init.c
@@ -77,8 +77,8 @@ void MX_Init()
 static void MX_I2C1_Init(void)
 {
     hi2c1.Instance = I2C1;
-    hi2c1.Init.ClockSpeed = 100000;
-    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+    hi2c1.Init.ClockSpeed = 400000;
+    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_16_9;
     hi2c1.Init.OwnAddress1 = 0;
     hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
     hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLED;
