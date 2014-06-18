30bfad71f0cf5860025ed3037b0f9755fd025354
pezo <pezo@d1c6ea46-091f-4b3d-9d77-b4eb87a02c39>
2014-06-18 08:22:59 +0000

    * stm/impy: (console.c) Implement 'board start' command.   (string_en.h) Add unknown error string.
    Command is not tested because communication with the AD5933 doesn't seem to work reliably yet.

--- trunk/stm/include/strings_en.h
+++ trunk/stm/include/strings_en.h
@@ -21,6 +21,7 @@ const char* const txtUnknownCommand = "Unknown command.";
 const char* const txtUnknownSubcommand = "Unknown subcommand.";
 const char* const txtNotImplemented = "Not yet implemented.";
 const char* const txtUnknownOption = "Unknown option: ";
+const char* const txtOops = "An unknown error occurred, panic.";
 // board get, board set
 const char* const txtInvalidValue = "Invalid value for this argument: ";
 const char* const txtSetOnlyWhenIdle = "This value cannot be set while a sweep is running: ";
