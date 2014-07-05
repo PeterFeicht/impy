3b2b9efc3cf98438ff9d4ec33af5797b472e159a
Peter Feichtinger <shippo@gmx.at>
2014-07-05 08:25:06 +0000

    * stm/impy: (language.settings.xml) Update for new GNU ARM Eclipse plugin version.   (stm32f4xx_hal_conf.h, usbd_conf.h, usbd_vcp.c) Update to new HAL driver version.

--- stm/include/stm32f4xx_hal_conf.h
+++ stm/include/stm32f4xx_hal_conf.h
@@ -114,12 +114,27 @@ extern "C"
 #endif /* HSI_VALUE */
 
 /**
+  * @brief Internal Low Speed oscillator (LSI) value.
+  */
+#if !defined  (LSI_VALUE) 
+ #define LSI_VALUE  ((uint32_t)40000)    
+#endif /* LSI_VALUE */                      /*!< Value of the Internal Low Speed oscillator in Hz
+                                             The real value may vary depending on the variations
+                                             in voltage and temperature.  */
+/**
+  * @brief External Low Speed oscillator (LSE) value.
+  */
+#if !defined  (LSE_VALUE)
+ #define LSE_VALUE  ((uint32_t)32768)    /*!< Value of the External Low Speed oscillator in Hz */
+#endif /* LSE_VALUE */
+
+/**
  * @brief External clock source for I2S peripheral
  *        This value is used by the I2S HAL module to compute the I2S clock source
  *        frequency, this source is inserted directly through I2S_CKIN pad.
  */
 #if !defined  (EXTERNAL_CLOCK_VALUE)
-#define EXTERNAL_CLOCK_VALUE    ((uint32_t)12288000) /*!< Value of the Internal oscillator in Hz*/
+#define EXTERNAL_CLOCK_VALUE    ((uint32_t)12288000) /*!< Value of the External oscillator in Hz*/
 #endif /* EXTERNAL_CLOCK_VALUE */
 
 /* Tip: To avoid modifying this file each time you need to use different HSE,
@@ -130,6 +145,7 @@ extern "C"
  * @brief This is the HAL system configuration section
  */
 #define  VDD_VALUE                    ((uint32_t)3300) /*!< Value of VDD in mv */
+#define  TICK_INT_PRIORITY            ((uint32_t)0x0F) /*!< tick interrupt priority */
 #define  USE_RTOS                     0
 #define  PREFETCH_ENABLE              1
 #define  INSTRUCTION_CACHE_ENABLE     1
